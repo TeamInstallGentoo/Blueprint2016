74889dc33c812bc8e65f7121d9d96d1db69662e5
Max Krieger <themaxaxis@gmail.com>
2016-02-28 17:28:30 -0500

    rotation..?

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -8,9 +8,36 @@ require("styles/Nav.scss");
 class NavComponent extends React.Component {
 	componentDidMount() {
 		this.renderCanvas();
-		window.addEventListener("deviceorientation", (e) => {
-			console.log(e.alpha);
-		});
+		window.addEventListener("deviceorientation", function(eventData) {
+		// gamma: Tilting the device from left to right. Tilting the device to the right will result in a positive value.
+		// gamma: Het kantelen van links naar rechts in graden. Naar rechts kantelen zal een positieve waarde geven.
+			var tiltLR = eventData.gamma;
+
+		// beta: Tilting the device from the front to the back. Tilting the device to the front will result in a positive value.
+		// beta: Het kantelen van voor naar achteren in graden. Naar voren kantelen zal een positieve waarde geven.
+			var tiltFB = eventData.beta;
+
+		// alpha: The direction the compass of the device aims to in degrees.
+		// alpha: De richting waarin de kompas van het apparaat heen wijst in graden.
+			var dir = eventData.alpha;
+
+		// Call the function to use the data on the page.
+		// Roep de functie op om de data op de pagina te gebruiken.
+			deviceOrientationHandler(tiltLR, tiltFB, dir);
+		}, false);
+
+		function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+			document.getElementById("tiltLR").innerHTML = Math.ceil(tiltLR);
+			document.getElementById("tiltFB").innerHTML = Math.ceil(tiltFB);
+			document.getElementById("direction").innerHTML = Math.ceil(dir);
+
+			// Rotate the disc of the compass.
+			// Laat de kompas schijf draaien.
+			var compassDisc = this.refs.canvas;
+			compassDisc.style.webkitTransform = "rotate("+ dir +"deg)";
+			compassDisc.style.MozTransform = "rotate("+ dir +"deg)";
+			compassDisc.style.transform = "rotate("+ dir +"deg)";
+		}
 	}
 	renderCanvas() {
 		var ctx = this.refs.canvas.getContext("2d");
